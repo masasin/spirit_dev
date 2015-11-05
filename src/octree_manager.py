0a687400fb60562d22fe2f6de3263f78b38beba4
masasin <mshtsy+github@gmail.com>
2015-11-06 05:42:49 +0900

    Refactor into class.

--- src/octree_manager.py
+++ src/octree_manager.py
@@ -19,19 +19,49 @@ from sensor_msgs.msg import Image
 from octree import Octree
 
 
-class StateData(object):
+class State(object):
+    def __init__(self, pose, image):
+        self.pose = pose
+        self._position_precise = np.array([self.pose.position.x,
+                                           self.pose.position.y,
+                                           self.pose.position.z])
+        self.position = self._position_precise // 10 * 10  # Round to 1 cm
+        self.image = image
+        self.stamp = strftime("%Y-%m-%d %H:%M:%S",
+                              localtime(self.pose.header.stamp.to_time()))
+
+    def __repr__(self):
+        return "Frame ({}): {}".format(self._position_precise.tolist(),
+                                       self.stamp)
+
+
+class Manager(object):
     """
     Data container.
 
     """
     def __init__(self):
         self.clear()
+        self.octree = Octree((0, 0, 0), 10000)  # 10 m
+        rospy.Subscriber("/output/slow_image_raw", Image, self.image_callback)
+        rospy.Subscriber("/ardrone/pose", PoseStamped, self.pose_callback)
 
     def image_callback(self, image):
         self.image = image
+        if self.is_ready:
+            rospy.logdebug("Adding data")
+            self.octree.insert(State(self.pose, self.image))
+            self.clear()
 
     def pose_callback(self, pose):
         self.pose = pose
+        best = self.select_best_past_image()
+
+    def select_best_past_image(self):
+        """
+        Use the current pose to select the best image.
+
+        """
 
     @property
     def is_ready(self):
@@ -42,44 +72,13 @@ class StateData(object):
         self.pose = None
 
 
-class Node(object):
-    def __init__(self, state):
-        self.pose = state.pose
-        self._position_precise = np.array([self.pose.position.x,
-                                           self.pose.position.y,
-                                           self.pose.position.z])
-        self.position = self._position_precise // 10 * 10  # Round to 1 cm
-        self.image = state.image
-        self.stamp = strftime("%Y-%m-%d %H:%M:%S",
-                              localtime(self.pose.header.stamp.to_time()))
-
-    def __repr__(self):
-        return "Frame ({}): {}".format(self._position_precise.tolist(),
-                                       self.stamp)
-
-
-def manager():
-    state = StateData()
-    octree = Octree((0, 0, 0), 10000)
-
+def main():
+    """Initialize ROS node."""
     rospy.init_node("manager")
-    rate = rospy.rate(200)  # Hz
-
-    rospy.Subscriber("/output/slow_image_raw", Image, state.image_callback)
-    rospy.Subscriber("/ardrone/pose", PoseStamped, state.pose_callback)
-
-    while not rospy.is_shutdown():
-        if state.is_ready:
-            rospy.logdebug("Adding data")
-            node = Node(state)
-            octree.insert(node)
-            state.clear()
-
-        rate.sleep()
+    Manager()
+    rospy.loginfo("Started octree manager")
+    rospy.spin()
 
 
 if __name__ == "__main__":
-    try:
-        manager()
-    except rospy.ROSInterruptException:
-        pass
+    main()
