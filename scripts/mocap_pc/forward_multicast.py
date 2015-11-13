55c9634ee95f22441a1756a944a3cfb91bab0cdf
masasin <mshtsy+github@gmail.com>
2015-11-13 18:09:01 +0900

    Fix header docstring.

--- scripts/mocap_pc/forward_multicast.py
+++ scripts/mocap_pc/forward_multicast.py
@@ -3,27 +3,34 @@
 # Released under BSD version 4
 """
 Listen to motion capture multicast and forward to an individual computer.
+
 For an unknown reason, I cannot find the required settings to stream position
 data from Optitrack's Motive:Tracker motion capture program. The base station,
 in ROS (specifically, the `mocap_optitrack` node, requires that data in order to
 work.
+
 To fix this, use the following settings in Motive:Tracker:
-    - Broadcast Frame Data is checked.
-    - Local Interface is the same as the cameras. (Ignore the warning.)
-    - Stream Markers is True
-    - Stream Rigid Bodies is True
-    - Remote Trigger is False
-    - Type is Multicast
-    - Command Port is 1510
-    - Data Port is 1511
-    - Multicast Interface is the same as the `MULTICAST_IP` variable below.
+
+- Broadcast Frame Data is checked.
+- Local Interface is the same as the cameras. (Ignore the warning.)
+- Stream Markers is True
+- Stream Rigid Bodies is True
+- Remote Trigger is False
+- Type is Multicast
+- Command Port is 1510
+- Data Port is 1511
+- Multicast Interface is the same as the `MULTICAST_IP` variable below.
+
 The hardware setup is as follows:
-    - Connect the cameras and the mocap computer to the same router using
-      ethernet
-    - Ensure that the base station can talk to the mocap computer (`ping` the
-      IP indicated in `ipconfig` for the mocap computer, and `ifconfig` for the
-      base station)
+
+- Connect the cameras and the mocap computer to the same router using
+  ethernet
+- Ensure that the base station can talk to the mocap computer (`ping` the IP
+  indicated in `ipconfig` for the mocap computer, and `ifconfig` for the base
+  station)
+
 Once this setup can be done, the `mocap_optitrack` node can run normally.
+
 """
 import socket
 import struct
