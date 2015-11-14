c0d3e637a477d8b79d8697c415c9bc3a352f59e0
masasin <mshtsy+github@gmail.com>
2015-11-14 14:26:07 +0900

    Add good documentation and comments.

--- src/beeper.py
+++ src/beeper.py
@@ -3,7 +3,11 @@
 # (C) 2015  Jean Nassar
 # Released under BSD version 4
 """
-Publish the tracking status of the AR.Drone.
+Publish the tracking status of the drone.
+
+Analyze the pose stream to ensure that the pose is being updated. If the pose
+does not change within `TIMEOUT`, then it is considered not to be tracked. If
+tracking was lost, beep to let the operator know.
 
 """
 import os
@@ -19,6 +23,19 @@ SPIRIT_ROOT = os.path.expanduser("~/catkin_ws/src/spirit")
 
 
 class TrackingVerifier(object):
+    """
+    Verify tracking.
+
+    Attributes
+    ----------
+    connected : bool
+        Whether a connection has been established.
+    last_updated : rospy.rostime.Time
+        The time at which the last change in pose occured.
+    tracking : bool
+        Whether the drone is being tracked. Initially, its value is None.
+
+    """
     def __init__(self):
         pygame.init()
         pygame.mixer.music.load(os.path.join(SPIRIT_ROOT, "audio/beep.wav"))
@@ -39,12 +56,21 @@ class TrackingVerifier(object):
 
     def beep(self):
         """
-        Beeps an audio file.
+        Plays an audio file containing a beep.
 
         """
         pygame.mixer.music.play()
 
     def callback(self, pose):
+        """
+        Receive pose updates and act accordingly.
+
+        Parameters
+        ----------
+        pose : StampedPose
+            A StampedPose containing the latest pose.
+
+        """
         if pose is None:
             rospy.logwarn("Received empty data")
             return
@@ -53,15 +79,17 @@ class TrackingVerifier(object):
             rospy.loginfo("Connection active")
             self.connected = True
 
-        if self._last_pose is not None:
-            # TODO (masasin): `self._last_pose.pose != pose.pose` always True
-            if self._last_pose.pose == pose.pose:
+        if self._last_pose is not None:  # Not the first update.
+            # TODO (masasin): `self._last_pose.pose != pose.pose` was always
+            #                 True in live tests. Cannot reproduce. Check to see
+            #                 if it is still the case.
+            if self._last_pose.pose == pose.pose:  # Pose unchanged.
                 reference_time = self.last_updated or self._start_time
                 if ((rospy.Time.now() - reference_time).to_sec() > TIMEOUT and
                         self.tracking is not False):  # Has a None init state
                     self.tracking = False
 
-            else:
+            else:  # Pose changed.
                 self.last_updated = pose.header.stamp
                 if not self.tracking:
                     self.tracking = True
@@ -70,10 +98,31 @@ class TrackingVerifier(object):
 
     @property
     def tracking(self):
+        """
+        Check whether the drone is being tracked.
+
+        Returns
+        -------
+        bool
+            True if the drone is being tracked.
+
+        """
         return self._tracking
 
     @tracking.setter
     def tracking(self, value):
+        """
+        Change the value of the tracking, and publish the new state.
+
+        Depending on the value and other class attributes, additional actions
+        may be taken.
+
+        Parameters
+        ----------
+        value : bool
+            The new value of the tracking state.
+
+        """
         if not value:
             if self.tracking is None:
                 rospy.logwarn("AR.Drone is not being tracked! "
@@ -97,6 +146,7 @@ def shutdown_hook():
 
 
 def main():
+    """Main entry point for script."""
     rospy.init_node("tracking_verifier", anonymous=True)
     rospy.on_shutdown(shutdown_hook)
     TrackingVerifier()
