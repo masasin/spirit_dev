5b735dff075cb4fb2e9fd89dc4d872281210964d
masasin <mshtsy+github@gmail.com>
2016-02-03 08:18:28 +0900

    Make compatible with Python 2 and 3.

--- config/regenerate_launch_files.py
+++ config/regenerate_launch_files.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 # (C) 2015  Jean Nassar
 # Released under BSD
 import glob
@@ -9,11 +9,11 @@ import rospkg
 import tqdm
 
 
-def get_launch_dir(package: str) -> str:
+def get_launch_dir(package):
     return os.path.join(rospkg.RosPack().get_path(package), "launch")
 
 
-def get_file_root(path: str) -> str:
+def get_file_root(path):
     """
     >>> get_file_root("/tmp/test.txt")
     'test'
@@ -22,19 +22,20 @@ def get_file_root(path: str) -> str:
     return os.path.split(path[:path.rindex(".")])[1]
 
 
-def compile_xacro(inpath: str, outpath: str) -> None:
+def compile_xacro(inpath, outpath, stdout):
     sp.call("rosrun xacro xacro {inpath} --inorder -o {outpath}"
             .format(inpath=inpath, outpath=outpath).split(),
-            stdout=sp.DEVNULL)
+            stdout=stdout)
 
 
 def main():
     launch_dir = get_launch_dir("spirit")
     os.chdir(launch_dir)
-    for path in tqdm.tqdm(glob.glob("xacro/*.xacro"),
-                          desc="Regenerating launch files"):
-        root = get_file_root(path)
-        compile_xacro(path, os.path.join("launchers", root))
+    with open(os.devnull, "w") as DEVNULL:
+        for path in tqdm.tqdm(glob.glob("xacro/*.xacro"),
+                              desc="Regenerating launch files"):
+            root = get_file_root(path)
+            compile_xacro(path, os.path.join("launchers", root), DEVNULL)
 
 
 if __name__ == "__main__":
