7d9bdf1d16f29448ca1ea8af1d179bddc389fb33
masasin <mshtsy+github@gmail.com>
2016-02-07 05:33:44 +0900

    Automatically update past_image launcher from parameters.

--- config/regenerate_launch_files.py
+++ config/regenerate_launch_files.py
@@ -2,15 +2,21 @@
 # (C) 2015  Jean Nassar
 # Released under BSD
 import glob
+from lxml import etree as et
 import os
 import subprocess as sp
 
 import rospkg
 import tqdm
+import yaml
 
 
-def get_launch_dir(package):
-    return os.path.join(rospkg.RosPack().get_path(package), "launch")
+namespace = {"xacro": "{http://www.ros.org/wiki/xacro}"}
+# et.register_namespace("xacro", "http://www.ros.org/wiki/xacro")
+
+
+def get_ros_dir(package, dirname):
+    return os.path.join(rospkg.RosPack().get_path(package), dirname)
 
 
 def get_file_root(path):
@@ -26,11 +32,62 @@ def compile_xacro(inpath, outpath, stdout):
     sp.call("rosrun xacro xacro {inpath} --inorder -o {outpath}"
             .format(inpath=inpath, outpath=outpath).split(),
             stdout=stdout)
+    
+
+def get_past_image_keys():
+    with open("launch_params.yaml") as fin:
+        launch_params = yaml.load(fin)
+
+    keys = {}
+    for eval_method in launch_params["past_image"]:
+        if eval_method != "eval_method":
+            keys[eval_method] = launch_params["past_image"][eval_method].keys()
+    return keys
+
+
+def load_xml(path):
+    parser = et.XMLParser(remove_blank_text=True)
+    return et.parse(path, parser)
+    
+
+def remove_old_elements(node):
+    for element in node.findall("{}if".format(namespace["xacro"])):
+        node.remove(element)
+
+
+def add_new_keys(node, keys):
+    for method_name, key_list in keys.items():
+        element = et.Element("{}if".format(namespace["xacro"]),
+                             attrib={"value": "${{method == '{}'}}"
+                                     .format(method_name)})
+        for key in key_list:
+            et.SubElement(element, "param",
+                          attrib={"name": key,
+                                  "value": "${{method_ns['{}']}}".format(key)})
+        node.append(element)
+
+
+def add_message(tree):
+    root = tree.getroot()
+    root.addprevious(et.Comment("Generated automatically from launch config file."))
+
+
+def update_past_image_generator(keys, path="xacro/past_images.launch.xacro"):
+    tree = load_xml(path)
+    for node in tree.findall("node[@name='past_image_selector']"):
+        remove_old_elements(node)
+        add_new_keys(node, keys)
+    add_message(tree)
+    tree.write(path, encoding="utf-8", xml_declaration=True, pretty_print=True)
 
 
 def main():
-    launch_dir = get_launch_dir("spirit")
-    os.chdir(launch_dir)
+    os.chdir(get_ros_dir("spirit", "config"))
+    past_image_keys = get_past_image_keys()
+
+    os.chdir(get_ros_dir("spirit", "launch"))
+    update_past_image_generator(past_image_keys)
+
     with open(os.devnull, "w") as DEVNULL:
         for path in tqdm.tqdm(glob.glob("xacro/*.xacro"),
                               desc="Regenerating launch files",
