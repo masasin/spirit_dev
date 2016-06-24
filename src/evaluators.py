02ac181445dfb2330701190faebe4d9c5465f9ee
masasin <mshtsy+github@gmail.com>
2016-06-24 20:01:46 +0900

    More documentation

--- src/evaluators.py
+++ src/evaluators.py
@@ -13,21 +13,74 @@ from helpers import Pose, Frame
 
 
 def get_evaluator(method, parent):
+    """
+    Get an evaluator.
+
+    Parameters
+    ----------
+    method : str
+        The name of the evaluator.
+    parent : Selector
+        The selector using the evaluator.
+
+    Returns
+    -------
+    Evaluator
+        The initialized evaluator.
+
+    """
     return getattr(sys.modules[__name__], method)(parent)
 
 
 class Evaluator(object):
+    """
+    Base class for evaluators.
+
+    The methods used and their coefficients can be set in the configuration file
+    by setting the ``coeff_{method_name}`` for the appropriate evaluator.
+
+    Parameters
+    ----------
+    parent : Selector
+        The selector using the evaluator.
+
+    """
     def __init__(self, parent):
         self._parent = parent
         self._vars_frame = {}
 
     def _evaluate_frame(self, pose, frame):
+        """
+        Evaluate the score for a pose against a frame.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The score for the pose against the frame.
+
+        """
         frame_score = sum(coeff * self.__getattribute__(component)(pose, frame)
                           for component, coeff in self.eval_coeffs.items())
         self._vars_frame = {}
         return frame_score
 
     def select_best_frame(self):
+        """
+        Select the best frame using the minimum of all individual frame scores.
+
+        Returns
+        -------
+        Frame
+            The best frame.
+
+        """
         if self.frames:
             if self.current_frame is None:
                 return self.frames[0]
@@ -66,27 +119,122 @@ class Evaluator(object):
 
 
 class ConstantTimeDelay(Evaluator):
+    """
+    Use an evaluator which keeps a constant time delay.
+
+    """
     def time(self, pose, frame):
+        """
+        Get the time difference between pose and frame.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The difference between the optimum and frame timestamps.
+
+        """
         optimum_timestamp = pose.header.stamp.to_sec() - self.ref_delay
         return abs(frame.stamp.to_sec() - optimum_timestamp)
 
 
 class ConstantDistance(Evaluator):
+    """
+    Use an evaluator which maintains a constant absolute distance.
+
+    """
     def distance(self, pose, frame):
+        """
+        Get the absolute distance between pose and frame.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The difference of the distance between frame and pose, and the
+            reference distance.
+
+        """
         return abs(frame.distance(pose) - self.ref_distance)
 
 
 class Spirit(Evaluator):
+    """
+    Use the evaluator from SPIRIT.
+
+    """
     @staticmethod
     def centrality(pose, frame):
+        """
+        Get how close to the centre of the frame the pose is.
+
+        If the pose is behind the frame, or the output is too high, cap.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The centrality score.
+
+        """
         dx, dy, dz = frame.rel_position(pose)
         return min((dx**2 + dz**2) / dy**2, 0.2) if dy < 0 else 0.2
 
     @staticmethod
     def direction(pose, frame):
+        """
+        Get how close the yaws of pose and frame are.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The direction score.
+
+        """
         return frame.rel_euler(pose)[2] ** 2
 
     def distance(self, pose, frame):
+        """
+        Get the closeness to the reference distance.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The distance score.
+
+        """
         return ((frame.distance(pose) - self.ref_distance)
                 / self.ref_distance)**2
 
@@ -97,7 +245,7 @@ class Konishi(Evaluator):
 
 class Murata(Evaluator):
     """
-    Use the evaluation function from Murata's thesis. [#]_
+    Use the evaluator from Murata's thesis. [#]_
 
     Extended Notes
     --------------
@@ -128,13 +276,44 @@ class Murata(Evaluator):
         super(Murata, self)._evaluate_frame(pose, frame)
 
     def height(self, pose, frame):
+        """
+        Get the closeness to a reference height.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The height score.
+
+        """
         return ((self._frame_vars["dzg"] - self.ref_height)
                 / self.ref_height)**2
 
     @staticmethod
     def direction(pose, frame):
-        beta = frame.rel_euler(pose)[2]
-        return beta**2
+        """
+        Get how close the yaws of pose and frame are.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The direction score.
+
+        """
+        return frame.rel_euler(pose)[2] ** 2
 
     def elevation(self, pose, frame):
         """
@@ -156,5 +335,21 @@ class Murata(Evaluator):
         return np.arctan2(self._frame_vars["dzg"], self._frame_vars["dyg"]) ** 2
 
     def distance(self, pose, frame):
+        """
+        Get the closeness to the reference distance.
+
+        Parameters
+        ----------
+        pose : Pose
+            The pose to be evaluated.
+        frame : Frame
+            The frame against which the pose is evaluated.
+
+        Returns
+        -------
+        float
+            The distance score.
+
+        """
         return ((frame.distance(pose) - self.ref_distance)
                 / self.ref_distance)**2
