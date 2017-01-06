e21589e3d940c9a2dd71251231e85ea369c79dc3
Jean Nassar <mshtsy+github@gmail.com>
2017-01-06 22:51:01 +0900

    Add tracking status message.

--- spirit/src/qt_display_video.py
+++ spirit/src/qt_display_video.py
@@ -15,6 +15,7 @@ from threading import Lock
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 import rospy
+from std_msgs.msg import Bool
 from sensor_msgs.msg import Image
 from ardrone_autonomy.msg import Navdata
 
@@ -40,36 +41,41 @@ class DroneVideoDisplay(QtWidgets.QMainWindow):
     }
     msg_disconnected = "Disconnected"
     msg_unknown = "Unknown State"
-    msg_status_template = "{state} (Battery: {battery:.0f}%)"
+    msg_status_template = "{state} (Battery: {battery:.0f}%){tracked}"
 
     def __init__(self):
         super(DroneVideoDisplay, self).__init__()
 
-        # Setup GUI - a label which fills the whole window and holds our image
+        # Setup GUI - a label which fills the whole window and holds our image.
         self.setWindowTitle(rospy.get_param("~window_name",
                                             "AR.Drone Video Feed"))
         self.image_box = QtWidgets.QLabel(self)
         self.setCentralWidget(self.image_box)
 
-        rospy.Subscriber("/ardrone/navdata", Navdata, self.receive_navdata)
-        rospy.Subscriber("image", Image, self.receive_image, queue_size=1)
+        rospy.Subscriber("/ardrone/navdata", Navdata, self.cbk_navdata)
+        rospy.Subscriber("/ardrone/tracked", Bool, self.cbk_tracked,
+                         queue_size=1)
+        rospy.Subscriber("image", Image, self.cbk_image, queue_size=1)
 
-        # Hold image frame received from drone for processing by GUI
+        # Holds drone tracking status.
+        self.tracked = False
+
+        # Holds image frame received from drone for processing by GUI.
         self.image = None
         self._image_lock = Lock()
 
-        # Holds the status message to be displayed on the next GUI update
+        # Holds the status message to be displayed on the next GUI update.
         self.msg_status_bar = ""
 
-        # Tracks whether we have received data since the last connection check
+        # Tracks whether we have received data since the last connection check.
         self._comm_since_timer = False
 
-        # A timer to check whether we"re still connected
+        # A timer to check whether we"re still connected.
         self.timer_connection = QtCore.QTimer(self)
         self.timer_connection.timeout.connect(self.cbk_connection)
         self.timer_connection.start(CONNECTION_CHECK_PERIOD)
 
-        # A timer to redraw the GUI
+        # A timer to redraw the GUI.
         self.timer_redraw = QtCore.QTimer(self)
         self.timer_redraw.timeout.connect(self.cbk_redraw)
         self.timer_redraw.start(GUI_UPDATE_PERIOD)
@@ -92,7 +98,7 @@ class DroneVideoDisplay(QtWidgets.QMainWindow):
     def cbk_redraw(self):
         if self.image is not None:
             with self._image_lock:
-                # Convert the ROS image into a QImage which we can display
+                # Convert the ROS image into a QImage which we can display.
                 image = QtGui.QPixmap.fromImage(
                     QtGui.QImage(self.image.data, self.image.width,
                                  self.image.height, QtGui.QImage.Format_RGB888))
@@ -101,26 +107,30 @@ class DroneVideoDisplay(QtWidgets.QMainWindow):
             self.resize(image.width(), image.height())
             self.image_box.setPixmap(image)
 
-        # Update the status bar
+        # Update the status bar.
         self.statusBar().showMessage(
             self.msg_status_bar if self.is_connected
             else self.msg_disconnected)
 
-    def receive_image(self, data):
+    def cbk_image(self, data):
         self._comm_since_timer = True
 
         with self._image_lock:
-            self.image = data  # Save ros image for processing by display thread
+            self.image = data  # Save ros image for display thread processing.
 
-    def receive_navdata(self, navdata):
+    def cbk_navdata(self, navdata):
         self._comm_since_timer = True
 
-        # Update the message to be displayed
+        # Update the message to be displayed.
         state = self.state_messages.get(navdata.state, self.msg_unknown)
         self.msg_status_bar = self.msg_status_template.format(
             state=state,
             battery=navdata.batteryPercent,
+            tracked="" if self.tracked else " TRACKING_LOST"
         )
+
+        def cbk_tracked(self, tracked):
+            self.tracked = tracked.data
 
 
 def main():
